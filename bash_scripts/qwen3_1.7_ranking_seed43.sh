8fe374acae643a8835e4648329ce639765f8687c
friendshipkim <kwj962004@gmail.com>
2025-09-06 16:25:39 +0000

    push 1.7b scripts with different seeds

--- bash_scripts/qwen3_1.7_ranking_seed43.sh
+++ bash_scripts/qwen3_1.7_ranking_seed43.sh
@@ -1,9 +1,9 @@
 export CUDA_VISIBLE_DEVICES=0
-export PORT=29502
+export PORT=29500
 
 ACCELERATE_LOG_LEVEL=info accelerate launch \
     --config_file recipes/accelerate_configs/zero3_singlegpu.yaml \
     --main_process_port $PORT \
     --num_processes=1 src/open_r1/grpo.py \
-    --config recipes/Qwen3-8B/grpo/config_chat_ranking.yaml \
+    --config recipes/Qwen3-1.7B/grpo/config_chat_ranking_seed43.yaml \
     --vllm_mode="colocate"
--- bash_scripts/qwen3_8_regular.sh
+++ bash_scripts/qwen3_8_regular.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=0
 export PORT=29503
 
 ACCELERATE_LOG_LEVEL=info accelerate launch \
