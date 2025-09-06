1567832de396b07b5a98da55d45c404b6e877440
friendshipkim <kwj962004@gmail.com>
2025-09-06 15:17:21 +0000

    add scripts

---
+++ bash_scripts/qwen3_8_regular.sh
@@ -0,0 +1,9 @@
+export CUDA_VISIBLE_DEVICES=3
+export PORT=29503
+
+ACCELERATE_LOG_LEVEL=info accelerate launch \
+    --config_file recipes/accelerate_configs/zero3_singlegpu.yaml \
+    --main_process_port $PORT \
+    --num_processes=1 src/open_r1/grpo.py \
+    --config recipes/Qwen3-8B/grpo/config_chat_regular.yaml \
+    --vllm_mode="colocate"
