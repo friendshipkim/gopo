33763cd3ec7292d2bc2971685dd13822827d6ade
friendshipkim <kwj962004@gmail.com>
2025-09-08 04:33:08 +0000

    add qwen3 4b scripts

---
+++ bash_scripts/qwen3_4_ranking_seed43.sh
@@ -0,0 +1,9 @@
+export CUDA_VISIBLE_DEVICES=0
+export PORT=29500
+
+ACCELERATE_LOG_LEVEL=info accelerate launch \
+    --config_file recipes/accelerate_configs/zero3_singlegpu.yaml \
+    --main_process_port $PORT \
+    --num_processes=1 src/open_r1/grpo.py \
+    --config recipes/Qwen3-4B/grpo/config_chat_ranking_seed43.yaml \
+    --vllm_mode="colocate"
