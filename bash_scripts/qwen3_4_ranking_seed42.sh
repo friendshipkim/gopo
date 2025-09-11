6023c940395a8f9a9f91e0373d6ce5ad86025849
friendshipkim <kwj962004@gmail.com>
2025-09-11 02:11:24 +0000

    run 4b with 4 gpus

--- bash_scripts/qwen3_4_ranking_seed42.sh
+++ bash_scripts/qwen3_4_ranking_seed42.sh
@@ -1,9 +1,9 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 export PORT=29500
 
 ACCELERATE_LOG_LEVEL=info accelerate launch \
-    --config_file recipes/accelerate_configs/zero3_singlegpu.yaml \
+    --config_file recipes/accelerate_configs/zero3_4gpus.yaml \
     --main_process_port $PORT \
-    --num_processes=1 src/open_r1/grpo.py \
+    --num_processes=4 src/open_r1/grpo.py \
     --config recipes/Qwen3-4B/grpo/config_chat_ranking_seed42.yaml \
     --vllm_mode="colocate"
--- bash_scripts/qwen3_4_regular_seed42.sh
+++ bash_scripts/qwen3_4_regular_seed42.sh
@@ -1,9 +1,9 @@
-export CUDA_VISIBLE_DEVICES=0
-export PORT=29500
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+export PORT=29501
 
 ACCELERATE_LOG_LEVEL=info accelerate launch \
-    --config_file recipes/accelerate_configs/zero3_singlegpu.yaml \
+    --config_file recipes/accelerate_configs/zero3_4gpus.yaml \
     --main_process_port $PORT \
-    --num_processes=1 src/open_r1/grpo.py \
+    --num_processes=4 src/open_r1/grpo.py \
     --config recipes/Qwen3-4B/grpo/config_chat_regular_seed42.yaml \
     --vllm_mode="colocate"
