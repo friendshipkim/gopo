a45ed44a12cfe4126adf00978cac3fc93deedd00
friendshipkim <kwj962004@gmail.com>
2025-09-15 20:14:30 +0000

    add qwen3 8b tldr scripts

---
+++ bash_scripts/qwen3_8_regular_seed42_tldr.sh
@@ -0,0 +1,9 @@
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+export PORT=29501
+
+ACCELERATE_LOG_LEVEL=info accelerate launch \
+    --config_file recipes/accelerate_configs/zero3_4gpus.yaml \
+    --main_process_port $PORT \
+    --num_processes=4 src/open_r1/grpo.py \
+    --config recipes/Qwen3-8B/grpo/config_tldr_regular_skywork-8b_seed42.yaml \
+    --vllm_mode="colocate"
