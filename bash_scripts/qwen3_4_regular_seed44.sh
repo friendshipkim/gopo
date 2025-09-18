92177a5bb99bc1d556dfca1452e17002edf512a9
friendshipkim <kwj962004@gmail.com>
2025-09-18 19:44:13 +0000

    add scripts

---
+++ bash_scripts/qwen3_4_regular_seed44.sh
@@ -0,0 +1,9 @@
+export CUDA_VISIBLE_DEVICES=4,5,6,7
+export PORT=29501
+
+ACCELERATE_LOG_LEVEL=info accelerate launch \
+    --config_file recipes/accelerate_configs/zero3_4gpus.yaml \
+    --main_process_port $PORT \
+    --num_processes=4 src/open_r1/grpo.py \
+    --config recipes/Qwen3-4B/grpo/config_chat_regular_seed44.yaml \
+    --vllm_mode="colocate"
