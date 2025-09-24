14f214251ad51e8f7c349e3d412d980ac7579c6a
friendshipkim <kwj962004@gmail.com>
2025-09-24 00:38:37 +0000

    update 4b scripts

--- bash_scripts/qwen3_4_ranking_seed42_sg.sh
+++ bash_scripts/qwen3_4_ranking_seed42_sg.sh
@@ -1,9 +1,9 @@
-export CUDA_VISIBLE_DEVICES=0
-export PORT=29500
+export CUDA_VISIBLE_DEVICES=0,1
+export PORT=29501
 
 ACCELERATE_LOG_LEVEL=info accelerate launch \
-    --config_file recipes/accelerate_configs/zero3_singlegpu.yaml \
+    --config_file recipes/accelerate_configs/zero3_2gpus.yaml \
     --main_process_port $PORT \
-    --num_processes=1 src/open_r1/grpo.py \
+    --num_processes=2 src/open_r1/grpo.py \
     --config recipes/Qwen3-4B/grpo/config_sg_ranking_skywork-8b_seed42.yaml \
     --vllm_mode="colocate"
